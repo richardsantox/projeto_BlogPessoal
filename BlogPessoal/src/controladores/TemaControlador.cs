d121764261fecd76478d4bbb2036a65f7fa50166
richardsantox <richardsantospereira4322@gmail.com>
2022-05-10 16:19:19 -0300

    @richardsantox [Task: Protegendo Aplicação] {10/05/2022}

---
+++ BlogPessoal/src/controladores/TemaControlador.cs
@@ -0,0 +1,101 @@
+﻿using BlogPessoal.src.dtos;
+using BlogPessoal.src.repositorios;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogPessoal.src.controladores
+{
+
+    [ApiController]
+    [Route("api/Temas")]
+    [Produces("application/json")]
+    public class TemaControlador : ControllerBase
+    {
+
+        #region Atributos
+
+        private readonly ITema _repositorio;
+
+        #endregion
+
+
+        #region Construtores
+
+        public TemaControlador(ITema repositorio)
+        {
+            _repositorio = repositorio;
+        }
+        
+        #endregion
+
+
+        #region Métodos
+
+        [HttpGet]
+        [Authorize]
+        public IActionResult PegarTodosTemas()
+        {
+            var lista = _repositorio.PegarTodosTemas();
+
+            if(lista.Count < 1) return NoContent();
+
+            return Ok(lista);
+        }
+
+
+        [HttpGet("id/idtema")]
+        [Authorize]
+        public IActionResult PegarTemaPeloId([FromRoute] int idtema)
+        {
+            var tema = _repositorio.PegarTemaPeloId(idtema);
+
+            if(tema == null) return NotFound();
+
+            return Ok(tema);
+        }
+
+
+        [HttpGet]
+        [Authorize]
+        public IActionResult PegarTemaPelaDescricao([FromQuery] string descricao)
+        {
+            var tema = _repositorio.PegarTemaPelaDescricao(descricao);
+
+            if(tema.Count < 1) return NoContent();
+
+            return Ok(tema);
+        }
+
+
+        [HttpPost]
+        [Authorize]
+        public IActionResult NovoTema([FromBody] NovoTemaDTO tema)
+        {
+            if (!ModelState.IsValid) return BadRequest();
+
+            _repositorio.NovoTema(tema);
+            return Created("api/Temas", tema);
+        }
+
+
+        [HttpPut]
+        [Authorize(Roles = "ADMINISTRADOR")]
+        public IActionResult AtualizarTema([FromBody] AtualizarTemaDTO tema)
+        {
+            if (!ModelState.IsValid) return BadRequest();
+
+            _repositorio.AtualizarTema(tema);
+            return Ok(tema);
+        }
+
+
+        [HttpDelete("deletar/{idTema}")]
+        [Authorize(Roles = "ADMINISTRADOR")]
+        public IActionResult DeletarTema([FromRoute] int idTema)
+        {
+            _repositorio.DeletarTema(idTema);
+            return NoContent();
+        }
+        #endregion
+    }
+}
---
+++ BlogPessoal/src/servicos/IAutenticacao.cs
@@ -0,0 +1,14 @@
+﻿using BlogPessoal.src.dtos;
+using BlogPessoal.src.modelo;
+
+namespace BlogPessoal.src.servicos
+{
+    public interface IAutenticacao
+    {
+        string CodificarSenha(string senha);
+        void CriarUsuarioSemDuplicar(NovoUsuarioDTO usuario);
+        string GerarToken(UsuarioModelo usuario);
+        AutorizacaoDTO PegarAutorizacao(AutenticarDTO autenticacao);
+    }
+
+}
